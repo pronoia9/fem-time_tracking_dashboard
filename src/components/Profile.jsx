4816b45d76b9fec8333f4090aea5c9cb19337c76
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-30 21:33:38 -0400

    profile widget done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,25 @@
 import './Profile.scss';
+const url = require('../assets/images/image-jeremy.png');
 
-export default function Profile() {
-  return <></>;
+export default function Profile({ user }) {
+  const { name } = user;
+
+  return (
+    <section className='dashboard__widget dashboard__profile bg-dark_blue'>
+      <div className='profile__header bg-primary'>
+        <img src={url} alt='profile pic' className='profile-image' />
+        <div className='profile__info'>
+          <p className='text-pale-blue'>Reports for</p>
+          <h2>{name}</h2>
+        </div>
+      </div>
+      <div className='profile__footer text-desaturated-blue'>
+        <p className='active' id='daily'>
+          Daily
+        </p>
+        <p id='weekly'>Weekly</p>
+        <p id='monthly'>Monthly</p>
+      </div>
+    </section>
+  );
 }
