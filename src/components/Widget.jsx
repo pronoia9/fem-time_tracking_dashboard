828d29ff84875c5fa2a815e7844e2fec202a7f2b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-30 21:48:01 -0400

    send state to time as prop

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -4,7 +4,7 @@ import './Widget.scss';
 
 export default function Widget({ data, state, changeTime }) {
   return data ? (
-    <Time data={data} />
+    <Time data={data} state={state} />
   ) : (
     <Profile state={state} changeTime={changeTime} user={{ name: 'Jeremy Robson', image: '../assets/images/image-jeremy.png' }} />
   );
