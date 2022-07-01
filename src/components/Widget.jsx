b9e25366e924774263a906356b510cbd58e6a523
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-30 21:41:28 -0400

    send changeTime to profile as prop

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -2,6 +2,10 @@ import Profile from './Profile';
 import Time from './Time';
 import './Widget.scss';
 
-export default function Widget({ data }) {
-  return data ? <Time data={data} /> : <Profile user={{ name: 'Jeremy Robson', image: '../assets/images/image-jeremy.png' }} />;
+export default function Widget({ data, changeTime }) {
+  return data ? (
+    <Time data={data} />
+  ) : (
+    <Profile changeTime={changeTime} user={{ name: 'Jeremy Robson', image: '../assets/images/image-jeremy.png' }} />
+  );
 }
