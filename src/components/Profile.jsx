0feb10986e328c052e352c3e40ac0836a1b4f80f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-30 22:37:14 -0400

    update img url

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,13 @@
 import './Profile.scss';
-const url = require('../assets/images/image-jeremy.png');
 
 export default function Profile({ user, state, changeTime }) {
-  const { name } = user;
+  const { name, image } = user;
   const times = [{ time: 'Daily' }, { time: 'Weekly' }, { time: 'Monthly' }];
 
   return (
     <section className='dashboard__widget dashboard__profile bg-dark_blue'>
       <div className='profile__header bg-primary'>
-        <img src={url} alt='profile pic' className='profile-image' />
+        <img src={require(`../assets/images/${image}`)} alt='profile pic' className='profile-image' />
         <div className='profile__info'>
           <p className='tc-pale-blue'>Reports for</p>
           <h2>{name}</h2>
