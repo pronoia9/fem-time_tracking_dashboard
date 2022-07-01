9cae73b97253d88eca8ccbce5766f5fcd052c433
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-30 21:08:49 -0400

    render time / profile component depending on data

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,5 +1,7 @@
+import Profile from './Profile';
+import Time from './Time';
 import './Widget.scss';
 
-export default function Widget() {
-  return <></>;
+export default function Widget({ data }) {
+  return data ? <Time data={data} /> : <Profile />;
 }
